96c0ca68c4601ac2a30fd6bf0c59fc5fa06ff6cc
Riyaa07 <RS374@myscc.ca>
2022-05-23 18:14:39 -0400

    created components and defined 5 arrays

---
+++ R_Shah_Food/src/app/content-list/content-list.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import{Content} from '../models/content'
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent implements OnInit {
+  
+
+  // constructor() { }
+
+  // ngOnInit(): void {
+  // }
+  Food:Content[];
+
+  constructor() {
+
+    this.Food = [{
+      id:0,
+      name: 'Ice cream',
+      title: 'Baskin Robbins',
+      body: 'It is an cotton candy and rocky roads',
+      author: 'Riya Shah',
+      imageLink: 'https://5.imimg.com/data5/SELLER/Default/2020/10/SB/SU/BK/23510599/baskins-and-robbins-500x500.jpg',
+      type: 'Icecream',
+      hashtags:['summertime']
+    }, {
+      id:1,
+      name: 'Nachos',
+      title: '3 Aminos resturant',
+      body: 'It is an mexican food served with sauce',
+      author: 'Riya Shah',
+      imageLink: 'https://img.restaurantguru.com/rc8d-3-Amigos-Boucherville-meals.jpg',
+      type: 'Mexican',
+      hashtags:['foood']
+    }, {
+      id:2,
+      name: 'Waffle',
+      title: 'Snowflake Luxary Gelato',
+      body: 'It is an made up of berries nad maple syrup',
+      author: 'Riya Shah',
+      imageLink: 'https://d1ralsognjng37.cloudfront.net/16b8216c-69cc-4cd6-8fbd-620b1a03c0f7.jpeg',
+      type: 'Icecreams',
+      hashtags:['lodondiaries']
+    }, {
+      id:3,
+      name: 'Manchurian sizzler',
+      title: 'Kyuchina Restro Cafe',
+      body: 'It is an made up of fried rice , fries, manchurian and panner',
+      author: 'Riya Shah',
+      imageLink: 'https://www.cityshor.com/uploads/article/07_2017/1499942978_web_5.jpg',
+      type: 'Indo-Chinese',
+      hashtags:['indianfood']
+    },{
+      id:4,
+      name: 'Strawberry cheesecake',
+      title: 'Moms Cafe',
+      body: 'It is an made up of cream cheese and strawberry and vanilla',
+      author: 'Riya Shah',
+      imageLink: 'https://www.thespruceeats.com/thmb/NOA0gyPkqU0Tu5kp-smimgROa7g=/1422x1422/smart/filters:no_upscale()/strawberry-cheesecake-4588014-03-5a5769c5f121456c8b4aa5c7c6e06f61.jpg',
+      type: 'Cakes',
+      hashtags:['pastry']
+    }];
+
+  }
+  ngOnInit(): void {
+    }
+}
+
+
