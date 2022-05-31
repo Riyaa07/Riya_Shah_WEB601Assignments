6ba88010fdb4a00a0774b97e0c7c37dc8dfeb995
Riyaa07 <RS374@myscc.ca>
2022-05-31 16:16:32 -0400

    Css completed

--- R_Shah_Food/src/app/cuisine.pipe.ts
+++ R_Shah_Food/src/app/cuisine.pipe.ts
@@ -9,7 +9,7 @@ export class CuisinePipe implements PipeTransform {
   transform(FoodItem: Content[], defaultvalue?: string): Content[] {
     return  FoodItem.filter(cuisine =>{
       return !cuisine.type?.length 
-       || cuisine.type == "Icecream" || cuisine.type =="Mexican" || cuisine.type =="Icecreams"  || cuisine.type =="Icecreams" || cuisine.type =="Indo-Chinese" || cuisine.type =="Sabji"|| cuisine.type =="Chinese";
+       || cuisine.type == "Chinese" || cuisine.type =="Desserts" ;
 
     })
   }
