15cb9f844486a7da18ade95b32a5d78f16807188
Riyaa07 <RS374@myscc.ca>
2022-05-30 22:19:07 -0400

    Version and added content

---
+++ R_Shah_Food/src/app/content-card/content-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../models/content';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+
+  clickImage(){
+    console.log(`The id is ${this.FoodItem?.id} and author is ${this.FoodItem?.author}`)
+  }
+  @Input() FoodItem?: Content;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
+
+
