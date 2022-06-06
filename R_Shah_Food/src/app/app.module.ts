2ba02a2451bc04a60676d07c8830d7edee7c2804
Riyaa07 <RS374@myscc.ca>
2022-06-05 22:52:21 -0400

    created imp data

--- R_Shah_Food/src/app/app.module.ts
+++ R_Shah_Food/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { CuisinePipe } from './cuisine.pipe';
+import { HighLightImportantDataDirective } from './high-light-important-data.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentCardComponent,
-    CuisinePipe
+    CuisinePipe,
+    HighLightImportantDataDirective
   ],
   imports: [
     BrowserModule
---
+++ R_Shah_Food/src/app/high-light-important-data.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighLightImportantDataDirective } from './high-light-important-data.directive';
+
+describe('HighLightImportantDataDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighLightImportantDataDirective();
+    expect(directive).toBeTruthy();
+  });
+});
